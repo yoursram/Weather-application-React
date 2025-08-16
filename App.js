c308ced9d1415805c681c17156e8154b0ebf35f1
Ram Kumar <yoursram07@gmail.com>
2025-08-16 17:41:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Head from './components/head'
+import Body from './components/body'
+import Footer from './components/footer'
+import Info from './components/info'
+function App() {
+  return (
+    <div >
+      <div>
+      <Head/>
+      </div>
+      <div>
+      <Body/>
+      </div>
+    </div>
+  );
+}
+export default App;
